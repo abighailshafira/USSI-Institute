5ef4d0d42101c4e0ffbb5cf6a34be61960d0b734
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-10-21 10:20:56 +0700

    Update profile page
    Adding image slider

--- src/components/Profile/Trainer.jsx
+++ src/components/Profile/Trainer.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import Image1 from "../../assets/image/team-1.jpg";
-import Image2 from "../../assets/image/team-2.jpg";
-import Image3 from "../../assets/image/team-3.jpg";
+import Image1 from "../../assets/image/trainer-1.jpg";
+import Image2 from "../../assets/image/trainer-2.jpg";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -53,13 +52,13 @@ export default class Responsive extends Component {
       ],
     };
     return (
-      <section className="pt-20">
+      <section className="pt-24">
         {" "}
         <div className="container">
           <div className="w-full px-4">
             <div className="mx-auto text-center mb-10">
-              <h1 className="text-3xl font-bold mb-2 uppercase text-slate-800">Team UI</h1>
-              <h2 className="text-base text-slate-500">Kami siap membantu anda untuk lebih bersinergi</h2>
+              <h1 className="text-3xl font-bold mb-2 uppercase text-slate-800">Trainer UI</h1>
+              <h2 className="text-base text-slate-500">Didukung oleh pemateri yang kompeten dibidangnya</h2>
             </div>
           </div>
 
@@ -113,7 +112,7 @@ export default class Responsive extends Component {
             <div className="px-5 py-5">
               <div className="bg-white rounded-md shadow-lg">
                 <div className="relative overflow-hidden bg-no-repeat bg-cover">
-                  <img src={Image3} className="w-full rounded-t-lg" />
+                  <img src="https://mdbootstrap.com/img/new/avatars/6.jpg" className="w-full rounded-t-lg" />
                   <a href="#!">
                     <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"></div>
                   </a>
@@ -167,29 +166,6 @@ export default class Responsive extends Component {
                     <path
                       fill="#fff"
                       d="M0,96L48,128C96,160,192,224,288,240C384,256,480,224,576,213.3C672,203,768,213,864,202.7C960,192,1056,160,1152,128C1248,96,1344,64,1392,48L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-                    ></path>
-                  </svg>
-                </div>
-
-                <div className="p-5">
-                  <h5 className="text-lg font-bold mb-2">Halley Frank</h5>
-
-                  <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium accusamus voluptatum deleniti atque corrupti.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="px-5 py-5">
-              <div className="bg-white rounded-md shadow-lg">
-                <div className="relative overflow-hidden bg-no-repeat bg-cover">
-                  <img src="https://mdbootstrap.com/img/new/avatars/15.jpg" className="w-full rounded-t-lg" />
-                  <a href="#!">
-                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"></div>
-                  </a>
-                  <svg className="absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{ left: 0, bottom: 0 }}>
-                    <path
-                      fill="#fff"
-                      d="M0,288L48,256C96,224,192,160,288,160C384,160,480,224,576,213.3C672,203,768,117,864,85.3C960,53,1056,75,1152,69.3C1248,64,1344,32,1392,16L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
                     ></path>
                   </svg>
                 </div>
