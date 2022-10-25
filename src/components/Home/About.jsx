40f7b60ea839b147d6f5628b76a6d0eab170ba1d
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-10-25 10:53:03 +0700

    Update style

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -5,12 +5,12 @@ import Image2 from "../../assets/image/image3.jpg";
 const About = () => {
   return (
     <>
-      <section className="pt-36">
+      <section className="pt-14 md:pt-24">
         <div className="container">
           <div className="mb-16">
             <h2 className="font-semibold text-lg text-cyan-500">Tentang UI</h2>
             <h1 className="text-3xl font-bold mb-2 uppercase text-slate-800">USSI Institute Divisi Pelatihan dan Sertifikasi</h1>
-            <p className="text-lg text-slate-500 text-justify">
+            <p className="text-base md:text-lg text-slate-500 text-justify">
               USSI INSTITUTE merupakan sebuah divisi yang berada di bawah naungan PT. USSI Pinbuk Prima Software yang bergerak dalam bidang teknologi informasi. Divisi USSI INSTITUTE lebih berfokus pada pelatihan dan peningkatan kompetensi
               SDM baik internal maupun eksternal. Dengan mengadakan pelatihan dan sertifikasi yang diselenggarakan secara berkelanjutan untuk menunjang operasional pada perusahaan customer juga untuk meningkatkan kompetensi SDM.
             </p>
@@ -19,11 +19,11 @@ const About = () => {
           <div className="text-slate-800 text-center md:text-left px-4">
             <div className="grid md:grid-cols-2 gap-x-6 xl:gap-x-12 items-center mb-12">
               <div className="mb-6 md:mb-0">
-                <div className="relative overflow-hidden shadow-lg rounded-md">
-                  <img src={Image1} className="w-full" alt="USSI" />
+                <div className="group relative overflow-hidden shadow-lg rounded-md aspect-[4/3] hover:scale-95 transition-all duration-500">
+                  <img src={Image1} className="w-full h-full bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500 group-hover:rotate-12" alt="USSI" />
                   <a href="#">
                     <div
-                      className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                      className="absolute top-0 right-0 bottom-0 left-0 w-full h-full"
                       style={{
                         backgroundColor: `rgba(251, 251, 251, 0.2)`,
                       }}
@@ -32,12 +32,12 @@ const About = () => {
                 </div>
               </div>
 
-              <div className="mb-6 md:mb-0">
-                <h3 className="text-2xl font-bold mb-3">Tujuan Kami</h3>
+              <div>
+                <h3 className="text-2xl font-bold mb-3 text-slate-800">Tujuan Kami</h3>
 
                 <p className="text-slate-500 text-base">
-                  Tujuan yang kami harapkan setelah mengikuti pendidikan dan pelatihan yang diselenggarakan USSI INSTITUTE adalah untuk membangun sistem pelayanan prima, dengan memberikan layanan cepat, tepat, skala prioritas dan waktu
-                  tanggap yang sesuai mutu internasional.
+                  Tujuan yang kami harapkan setelah mengikuti pendidikan dan pelatihan yang diselenggarakan USSI INSTITUTE adalah untuk <b>membangun sistem pelayanan prima</b>, dengan memberikan layanan cepat, tepat, skala prioritas dan
+                  waktu tanggap yang sesuai mutu internasional.
                 </p>
               </div>
             </div>
@@ -46,11 +46,11 @@ const About = () => {
           <div className="text-slate-800 text-center md:text-right px-4">
             <div className="grid md:grid-cols-2 gap-x-6 xl:gap-x-12 items-center">
               <div className="mb-6 md:mb-0 md:order-2">
-                <div className="relative overflow-hidden shadow-lg rounded-md">
-                  <img src={Image2} className="w-full" alt="USSI" />
+                <div className="group relative overflow-hidden shadow-lg rounded-md aspect-[4/3] hover:scale-95 transition-all duration-500">
+                  <img src={Image2} className="w-full h-full bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500 group-hover:rotate-12" alt="USSI" />
                   <a href="#">
                     <div
-                      className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                      className="absolute top-0 right-0 bottom-0 left-0 w-full h-full"
                       style={{
                         backgroundColor: `rgba(251, 251, 251, 0.2)`,
                       }}
@@ -59,11 +59,11 @@ const About = () => {
                 </div>
               </div>
 
-              <div className="mb-6 md:mb-0 md:order-1">
-                <h3 className="text-2xl font-bold mb-3">Komitmen Kami</h3>
+              <div className="md:order-1">
+                <h3 className="text-2xl font-bold mb-3 text-slate-800">Komitmen Kami</h3>
 
                 <p className="text-slate-500 text-base">
-                  Kami selaku vendor penyedia jasa corebanking system teknologi informasi memiliki komitmen untuk menciptakan sumberdaya yang handal, profesional, memiliki integritas, loyalitas dan jiwa kepemimpinan.
+                  Kami selaku vendor penyedia jasa corebanking system teknologi informasi memiliki komitmen untuk <b>menciptakan sumberdaya</b> yang handal, profesional, memiliki integritas, loyalitas dan jiwa kepemimpinan.
                 </p>
               </div>
             </div>
--- src/components/Home/Divisi.jsx
+++ src/components/Home/Divisi.jsx
@@ -1,46 +1,38 @@
 import React from "react";
+import Image1 from "../../assets/image/logo-audit.png";
+import Image2 from "../../assets/image/logo-ussi.png";
 
 const Divisi = () => {
   return (
     <>
-      <section className="py-24 mt-36 bg-slate-100">
-        <div className="container  text-slate-800 text-center">
+      <section className="py-36 mt-36 bg-slate-100">
+        <div className="container text-slate-800 text-center">
           <div className="grid lg:gap-x-12 lg:grid-cols-2">
-            <div className="mb-12 lg:mb-0">
+            <div className="mb-24 lg:mb-0">
               <div className="rounded-lg shadow-lg h-full block bg-white">
                 <div className="flex justify-center">
-                  <div className="p-4 bg-cyan-500 rounded-full shadow-lg inline-block -mt-8">
-                    <svg className="w-7 h-7 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                      <path
-                        fill="currentColor"
-                        d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z"
-                      ></path>
-                    </svg>
+                  <div className="p-8 bg-white rounded-full shadow-lg inline-block -mt-16">
+                    <img src={Image1} alt="USSI Institute" className="w-20" />
                   </div>
                 </div>
                 <div className="p-6">
-                  <h3 className="text-2xl font-bold text-cyan-500 mb-4">USSI Institute</h3>
-                  <p className="text-gray-500">Divisi</p>
-                  <h5 className="text-lg font-medium mb-4">Pelatihan dan Sertifikasi</h5>
+                  <h3 className="text-2xl font-bold text-cyan-500 mb-5">USSI Institute</h3>
+                  <p className="text-slate-500 text-base mb-0">Divisi</p>
+                  <h4 className="text-lg font-medium mb-4">Pelatihan dan Sertifikasi</h4>
                 </div>
               </div>
             </div>
 
             <div className="rounded-lg shadow-lg h-full block bg-white">
               <div className="flex justify-center">
-                <div className="p-4 bg-cyan-500 rounded-full shadow-lg inline-block -mt-8">
-                  <svg className="w-7 h-7 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                    <path
-                      fill="currentColor"
-                      d="M512.1 191l-8.2 14.3c-3 5.3-9.4 7.5-15.1 5.4-11.8-4.4-22.6-10.7-32.1-18.6-4.6-3.8-5.8-10.5-2.8-15.7l8.2-14.3c-6.9-8-12.3-17.3-15.9-27.4h-16.5c-6 0-11.2-4.3-12.2-10.3-2-12-2.1-24.6 0-37.1 1-6 6.2-10.4 12.2-10.4h16.5c3.6-10.1 9-19.4 15.9-27.4l-8.2-14.3c-3-5.2-1.9-11.9 2.8-15.7 9.5-7.9 20.4-14.2 32.1-18.6 5.7-2.1 12.1.1 15.1 5.4l8.2 14.3c10.5-1.9 21.2-1.9 31.7 0L552 6.3c3-5.3 9.4-7.5 15.1-5.4 11.8 4.4 22.6 10.7 32.1 18.6 4.6 3.8 5.8 10.5 2.8 15.7l-8.2 14.3c6.9 8 12.3 17.3 15.9 27.4h16.5c6 0 11.2 4.3 12.2 10.3 2 12 2.1 24.6 0 37.1-1 6-6.2 10.4-12.2 10.4h-16.5c-3.6 10.1-9 19.4-15.9 27.4l8.2 14.3c3 5.2 1.9 11.9-2.8 15.7-9.5 7.9-20.4 14.2-32.1 18.6-5.7 2.1-12.1-.1-15.1-5.4l-8.2-14.3c-10.4 1.9-21.2 1.9-31.7 0zm-10.5-58.8c38.5 29.6 82.4-14.3 52.8-52.8-38.5-29.7-82.4 14.3-52.8 52.8zM386.3 286.1l33.7 16.8c10.1 5.8 14.5 18.1 10.5 29.1-8.9 24.2-26.4 46.4-42.6 65.8-7.4 8.9-20.2 11.1-30.3 5.3l-29.1-16.8c-16 13.7-34.6 24.6-54.9 31.7v33.6c0 11.6-8.3 21.6-19.7 23.6-24.6 4.2-50.4 4.4-75.9 0-11.5-2-20-11.9-20-23.6V418c-20.3-7.2-38.9-18-54.9-31.7L74 403c-10 5.8-22.9 3.6-30.3-5.3-16.2-19.4-33.3-41.6-42.2-65.7-4-10.9.4-23.2 10.5-29.1l33.3-16.8c-3.9-20.9-3.9-42.4 0-63.4L12 205.8c-10.1-5.8-14.6-18.1-10.5-29 8.9-24.2 26-46.4 42.2-65.8 7.4-8.9 20.2-11.1 30.3-5.3l29.1 16.8c16-13.7 34.6-24.6 54.9-31.7V57.1c0-11.5 8.2-21.5 19.6-23.5 24.6-4.2 50.5-4.4 76-.1 11.5 2 20 11.9 20 23.6v33.6c20.3 7.2 38.9 18 54.9 31.7l29.1-16.8c10-5.8 22.9-3.6 30.3 5.3 16.2 19.4 33.2 41.6 42.1 65.8 4 10.9.1 23.2-10 29.1l-33.7 16.8c3.9 21 3.9 42.5 0 63.5zm-117.6 21.1c59.2-77-28.7-164.9-105.7-105.7-59.2 77 28.7 164.9 105.7 105.7zm243.4 182.7l-8.2 14.3c-3 5.3-9.4 7.5-15.1 5.4-11.8-4.4-22.6-10.7-32.1-18.6-4.6-3.8-5.8-10.5-2.8-15.7l8.2-14.3c-6.9-8-12.3-17.3-15.9-27.4h-16.5c-6 0-11.2-4.3-12.2-10.3-2-12-2.1-24.6 0-37.1 1-6 6.2-10.4 12.2-10.4h16.5c3.6-10.1 9-19.4 15.9-27.4l-8.2-14.3c-3-5.2-1.9-11.9 2.8-15.7 9.5-7.9 20.4-14.2 32.1-18.6 5.7-2.1 12.1.1 15.1 5.4l8.2 14.3c10.5-1.9 21.2-1.9 31.7 0l8.2-14.3c3-5.3 9.4-7.5 15.1-5.4 11.8 4.4 22.6 10.7 32.1 18.6 4.6 3.8 5.8 10.5 2.8 15.7l-8.2 14.3c6.9 8 12.3 17.3 15.9 27.4h16.5c6 0 11.2 4.3 12.2 10.3 2 12 2.1 24.6 0 37.1-1 6-6.2 10.4-12.2 10.4h-16.5c-3.6 10.1-9 19.4-15.9 27.4l8.2 14.3c3 5.2 1.9 11.9-2.8 15.7-9.5 7.9-20.4 14.2-32.1 18.6-5.7 2.1-12.1-.1-15.1-5.4l-8.2-14.3c-10.4 1.9-21.2 1.9-31.7 0zM501.6 431c38.5 29.6 82.4-14.3 52.8-52.8-38.5-29.6-82.4 14.3-52.8 52.8z"
-                    />
-                  </svg>
+                <div className="p-8 bg-white rounded-full shadow-lg inline-block -mt-16">
+                  <img src={Image1} alt="Audit Kepatuhan TI" className="w-20" />
                 </div>
               </div>
               <div className="p-6">
-                <h3 className="text-2xl font-bold text-cyan-500 mb-4">Audit Kepatuhan TI</h3>
-                <p className="text-gray-500">Divisi</p>
-                <h5 className="text-lg font-medium mb-4">Audit Kepatuhan Teknologi Informasi</h5>
+                <h3 className="text-2xl font-bold text-cyan-500 mb-5">Audit Kepatuhan TI</h3>
+                <p className="text-slate-500 text-base mb-0">Divisi</p>
+                <h4 className="text-lg font-medium mb-4">Audit Kepatuhan Teknologi Informasi</h4>
               </div>
             </div>
           </div>
