1467e33dd5a413c93e15ad7805b40e8905ea4dfa
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-10-27 11:47:00 +0700

    Update table style

--- src/components/ProfileUser/TabelKeterangan.jsx
+++ src/components/ProfileUser/TabelKeterangan.jsx
@@ -1,45 +1,46 @@
-import { Space, Table, Tag } from 'antd';
-import React from 'react';
+import { Space, Table, Tag } from "antd";
+import React from "react";
+
 const columns = [
   {
-    title: 'status',
-    dataIndex: 'status',
-    key: 'status',
+    title: "Status",
+    dataIndex: "status",
+    key: "status",
   },
   {
-    title: 'keterangan',
-    dataIndex: 'keterangan',
-    key: 'keterangan',
+    title: "Keterangan",
+    dataIndex: "keterangan",
+    key: "keterangan",
   },
 ];
+
 const data = [
   {
-    key: '1',
-    status: 'DOWNLOAD',
-    keterangan : 'Sertifikat belum di-download',
-
+    key: "1",
+    status: "DOWNLOAD",
+    keterangan: "Sertifikat belum di-download",
   },
   {
-    key: '2',
-    status: 'LULUS',
-    keterangan : 'Sertifikat masih dalam proses',
+    key: "2",
+    status: "LULUS",
+    keterangan: "Sertifikat masih dalam proses",
   },
   {
-    key: '3',
-    status: 'TIDAK LULUS',
-    keterangan : 'Tidak memperoleh sertifikat',
+    key: "3",
+    status: "TIDAK LULUS",
+    keterangan: "Tidak memperoleh sertifikat",
   },
   {
-    key: '3',
-    status: 'MENUNGGU HASIL',
-    keterangan : 'Menunggu hasil ujian',
+    key: "3",
+    status: "MENUNGGU HASIL",
+    keterangan: "Menunggu hasil ujian",
   },
 ];
-const App = () => 
+const App = () => (
   <>
-    <div className='container'>
-    <Table className='p-10 w-1/2 italic font-bold' pagination={false} columns={columns} dataSource={data} />
+    <div className="container">
+      <Table className="px-5 pt-10 w-1/2" pagination={false} columns={columns} dataSource={data} />
     </div>
-  </>;
+  </>
+);
 export default App;
-
