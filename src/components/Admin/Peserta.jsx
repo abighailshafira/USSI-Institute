6a06d115412ce1fa416b0cb8315e23e786df02e7
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-11-23 16:01:24 +0700

    Update Peserta.jsx

--- src/components/Admin/Peserta.jsx
+++ src/components/Admin/Peserta.jsx
@@ -2,82 +2,32 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/image/logo-ussi.png";
 import "antd/dist/antd.css";
-import { PlusOutlined } from "@ant-design/icons";
-import { Form, Input, Button, Layout, Menu, Modal, Space, Table } from "antd";
+import { Layout, Menu, Space, Form, Input, Button, Modal } from "antd";
 import Search from "antd/lib/transfer/search";
-import { FiHome, FiSettings, FiLogOut, FiUser, FiChevronLeft } from "react-icons/fi";
-import { HiOutlineOfficeBuilding, HiOutlineViewGridAdd } from "react-icons/hi";
-import { GrCertificate } from "react-icons/gr";
+import { PlusOutlined } from "@ant-design/icons";
+import { FiHome, FiList, FiSettings, FiLogOut, FiUser } from "react-icons/fi";
+import { HiOutlineOfficeBuilding, HiOutlineViewGrid, HiOutlineViewGridAdd } from "react-icons/hi";
+import { TbCertificate } from "react-icons/tb";
+import TablePeserta from "./Table/TablePeserta";
 
 const { Header, Content, Sider } = Layout;
 
 const Peserta = () => {
-  //modal
-  const [open, setOpen] = useState(false);
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const showModal = () => {
-    setOpen(true);
-  };
-
-  const handleOk = () => {
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setOpen(false);
-      setConfirmLoading(false);
-    }, 2000);
-  };
-
-  const handleCancel = () => {
-    console.log("Clicked cancel button");
-    setOpen(false);
-  };
-
-  //table
-  const columns = [
-    {
-      title: "Kode",
-      align: "center",
-      dataIndex: "code",
-      key: "code",
-      width: 50,
-      align: "center",
-    },
-    {
-      title: "Email",
-      align: "center",
-      dataIndex: "institutionAddress",
-      key: "institutionAddress",
-      width: 100,
-      align: "center",
-    },
-    {
-      title: "Actions",
-      align: "center",
-      dataIndex: "statusSLA",
-      key: "statusSLA",
-      width: 80,
-      align: "center",
-    },
-  ];
-
-  const data = [];
-  for (let i = 0; i < 10; i++) {
-    data.push({
-      key: i,
-    });
-  }
+  // Navbar
+  // const [collapsed, setCollapsed] = useState(false);
 
   return (
     <>
-      {" "}
       <Layout>
         <Sider
+          width={230}
           breakpoint="lg"
           collapsedWidth="0"
-          style={{
-            height: "screen",
-            // position: 'fixed',
-          }}
+          style={
+            {
+              // height: "screen",
+            }
+          }
           onBreakpoint={(broken) => {
             console.log(broken);
           }}
@@ -92,34 +42,42 @@ const Peserta = () => {
               </a>
             </Link>
           </div>
+
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={["3"]}
+            defaultSelectedKeys={["peserta"]}
             // selectedKeys={[location.pathname]}
           >
-            <Menu.Item key="1" icon={<FiHome />}>
+            <Menu.Item key="dashboard" icon={<FiHome />}>
               <Link to="/dashboard" />
               Dashboard
             </Menu.Item>
-            <Menu.Item key="2" icon={<HiOutlineViewGridAdd />}>
-              <Link to="/dashboard/pelatihan" />
-              Pelatihan
-            </Menu.Item>
-            <Menu.Item key="3" icon={<HiOutlineOfficeBuilding />}>
+            <Menu.SubMenu title="Pelatihan" icon={<HiOutlineViewGrid />}>
+              <Menu.Item key="pelatihan" icon={<HiOutlineViewGridAdd />}>
+                <Link to="/dashboard/pelatihan" />
+                Pelatihan
+              </Menu.Item>
+              <Menu.Item key="rekap pelatihan" icon={<FiList />}>
+                <Link to="/dashboard/rekap-pelatihan" />
+                Rekap Pelatihan
+              </Menu.Item>
+            </Menu.SubMenu>
+            <Menu.Item key="lembaga" icon={<HiOutlineOfficeBuilding />}>
               <Link to="/dashboard/lembaga" />
               Lembaga
             </Menu.Item>
-            <Menu.Item key="4" icon={<GrCertificate />}>
-              <Link to="/dashboard" />
+            <Menu.Item key="peserta" icon={<TbCertificate />}>
+              <Link to="/dashboard/peserta" />
               Peserta
             </Menu.Item>
-            <Menu.SubMenu key="SubMenu" title="User" icon={<FiUser />}>
+            <Menu.SubMenu title="Pengguna" icon={<FiUser />}>
               <Menu.Item key="one" icon={<FiSettings />}>
-                Profile
+                <Link to="/dashboard/profile" />
+                Profil
               </Menu.Item>
               <Menu.Item key="two" icon={<FiLogOut />}>
-                Logout
+                Keluar
               </Menu.Item>
             </Menu.SubMenu>
           </Menu>
@@ -127,12 +85,12 @@ const Peserta = () => {
 
         <Layout>
           <Header
-            className="site-layout-sub-header-background"
+            className="site-layout-background"
             style={{
               padding: 0,
             }}
           >
-            <Menu mode="horizontal">
+            <Menu mode="inline">
               <Space
                 style={{
                   marginLeft: 20,
@@ -145,6 +103,10 @@ const Peserta = () => {
                   size="large"
                   // onSearch={onSearch}
                 />
+                <Menu.Item key="1" icon={<FiHome />}>
+                  Hi, Admin
+                  <Link to="/dashboard" />
+                </Menu.Item>
               </Space>
             </Menu>
           </Header>
@@ -161,97 +123,23 @@ const Peserta = () => {
                 minHeight: 644,
               }}
             >
-              <div className="container p-3 mb-1 bg-body rounded d-flex flex-column">
-                <div className="bg-white shadow-lg rounded-lg p-10">
+              <div className="container p-3">
+                <div>
+                  <h1 className="text-3xl mb-2 text-slate-800">Peserta</h1>
+                  <p className="text-base text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis laudantium magnam quaerat?</p>
+                </div>
+
+                <div className="dashboard-card mt-10">
                   <div className="flex justify-between mb-3">
                     <div>
-                      <h1 className="title font-semibold text-3xl text-slate-800">Pengguna</h1>
+                      <h2 className="title font-semibold text-xl text-slate-800">Peserta</h2>
                     </div>
                     <div>
-                      <Button type="primary" icon={<PlusOutlined />} onClick={showModal} />
+                      <Button type="primary" icon={<PlusOutlined />} />
                     </div>
                   </div>
-
-                  <Table pagination={false} columns={columns} dataSource={data} bordered size="middle" />
+                 <TablePeserta />
                 </div>
-
-                <div className="bg-white shadow-lg rounded-lg p-10 mt-10">
-                  <h2 className="text-slate-800 flex items-center text-lg">
-                    <a href="#">
-                      <FiChevronLeft className="w-4 h-4 text-cyan-500 mr-2" />
-                    </a>
-                    Tambah Pengguna
-                  </h2>
-                  <Form
-                    labelCol={{
-                      span: 7,
-                    }}
-                    wrapperCol={{
-                      span: 14,
-                    }}
-                    layout="horizontal"
-                  >
-                    <Form.Item label="Kode">
-                      <Input />
-                    </Form.Item>
-                    <Form.Item label="Email">
-                      <Input />
-                    </Form.Item>
-                    <Form.Item label="Password">
-                      <Input />
-                    </Form.Item>
-                  </Form>
-                </div>
-
-                <div className="bg-white shadow-lg rounded-lg p-10 mt-10">
-                  <h2 className="text-slate-800 flex items-center text-lg">
-                    <a href="#">
-                      <FiChevronLeft className="w-4 h-4 text-cyan-500 mr-2" />
-                    </a>
-                    Edit Pengguna
-                  </h2>
-                  <Form
-                    labelCol={{
-                      span: 7,
-                    }}
-                    wrapperCol={{
-                      span: 14,
-                    }}
-                    layout="horizontal"
-                  >
-                    <Form.Item label="Kode">
-                      <Input />
-                    </Form.Item>
-                    <Form.Item label="Email">
-                      <Input />
-                    </Form.Item>
-                    <Form.Item label="Password">
-                      <Input />
-                    </Form.Item>
-                  </Form>
-                </div>
-
-                <Modal title="Detail Pengguna" open={open} onOk={handleOk} confirmLoading={confirmLoading} onCancel={handleCancel}>
-                  <Form
-                    labelCol={{
-                      span: 7,
-                    }}
-                    wrapperCol={{
-                      span: 14,
-                    }}
-                    layout="horizontal"
-                  >
-                    <Form.Item label="Kode">
-                      <Input />
-                    </Form.Item>
-                    <Form.Item label="Email">
-                      <Input />
-                    </Form.Item>
-                    <Form.Item label="Password">
-                      <Input />
-                    </Form.Item>
-                  </Form>
-                </Modal>
               </div>
             </div>
           </Content>
