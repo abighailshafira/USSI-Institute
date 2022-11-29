93c1eaa1c97ea7690fcccd4f6ef2371bbb8ef18a
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-11-29 18:22:41 +0700

    Integrasi tabel admin

--- src/components/Admin/Form/InfoPengguna.jsx
+++ src/components/Admin/Form/InfoPengguna.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Input } from "antd";
+import axios from "axios";
 
 const InfoPengguna = () => {
+  // Integrasi
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    getAdmin();
+  }, []);
+
+  const getAdmin = async () => {
+    await axios
+      .get("http://localhost:5000/api/v1/admin", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        const getData = res.data.data;
+        console.log(getData);
+        setUser(getData);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
       <Form
@@ -15,10 +38,10 @@ const InfoPengguna = () => {
           <Input disabled={true} />
         </Form.Item>
         <Form.Item name="name" label="Nama">
-          <Input disabled={true} />
+          <Input disabled={true} value={user.name} />
         </Form.Item>
         <Form.Item name="email" label="E-mail">
-          <Input disabled={true} />
+          <Input disabled={true} value={user.email} />
         </Form.Item>
       </Form>
     </>
